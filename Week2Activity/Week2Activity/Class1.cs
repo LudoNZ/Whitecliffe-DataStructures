48bd599a11de5f39705a39d4c5dbf05929daa6f7
ludo <ludobourneville@gmail.com>
2022-03-04 12:59:10 +1300

    first commit

---
+++ Week2Activity/Week2Activity/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Week2Activity
+{
+    class W2A1
+    {
+        public List<int> numbers = new List<int> { };
+
+        static int egg = 0;
+
+    
+    }
+}
---
+++ Week2Activity/Week2Activity/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Week2Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var list = new MyList();
+            List.Addnumber
+        }
+    }
+}
