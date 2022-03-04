579dc6fc15f131af45ddcbe37a3693d376663569
ludo <ludobourneville@gmail.com>
2022-03-04 16:51:58 +1300

    task 1 complete

--- Week2Activity/Week2Activity/Program.cs
+++ Week2Activity/Week2Activity/Program.cs
@@ -1,20 +1,17 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-namespace Week2Activity
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var list = new MyList();
-            List.Addnumber
+
+            Console.WriteLine("\nActivity1:");
+
+            activity1.Compute();
+
         }
     }
 }
---
+++ Week2Activity/Week2Activity/activity1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class activity1
+    {
+        static int[] numbers;
+
+        public static void Compute()
+        {
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Enter number { i + 1 } of 10:");
+                numbers[i] = Int32.Parse(Console.ReadLine());
+            }
+
+
+            Console.Write("Elements in array are: ");
+
+            for (int i = 0; i < 10; i++) Console.Write($" {numbers[i]}");
+
+            Console.ReadLine();
+        }
+    }
+}
